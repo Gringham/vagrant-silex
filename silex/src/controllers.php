f53671a14638cf31eab7e004e33f4a922b03d21b
chris <Jirachi11@web.de>
2016-01-25 14:44:35 +0100

    Added some new Documents

--- silex/src/controllers.php
+++ silex/src/controllers.php
@@ -1,16 +1,23 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
 
-$app->get('/welcome/{name}', function ($name) use ($app) {
+$app->get('/home', function () use ($app) {
     return $app['templating']->render(
-        'hello.html.php',
-        array('name' => $name)
+        'home.html.php',
+        array('active' => 'home')
     );
 });
 
-$app->get('/welcome-twig/{name}', function ($name) use ($app) {
-    return $app['twig']->render(
-        'hello.html.twig',
-        array('name' => $name)
+$app->get('/blog', function () use ($app) {
+    return $app['templating']->render(
+        'blog.html.php',
+        array('active' => 'blog')
+    );
+});
+
+$app->get('/new ', function () use ($app) {
+    return $app['templating']->render(
+        'new.html.php',
+        array('active' => 'new')
     );
 });
