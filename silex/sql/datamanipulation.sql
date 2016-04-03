aedbea0a9a8ebde5fcb7f2d4851100979a544eb4
christoph leiter <christoph.leiter@hpe.com>
2016-04-03 19:56:58 +0200

    Ein Semikolon wurde hinzugefügt

--- silex/sql/datamanipulation.sql
+++ silex/sql/datamanipulation.sql
@@ -2,4 +2,4 @@ USE silex;
 
 -- PREFILL YOUR TABLES HERE
 INSERT INTO blog_post (title, text) VALUES ('New Title', 'This is the content');
-INSERT INTO usrpwd (username, password) VALUES ('admin', '0000')
+INSERT INTO usrpwd (username, password) VALUES ('admin', '0000');
