6c02b2df83801810186937da9e9dda6022d38e7f
christoph leiter <christoph.leiter@hpe.com>
2016-04-03 19:46:31 +0200

    Ein Buchstabe zuviel wurde entfernt

--- silex/sql/datamanipulation.sql
+++ silex/sql/datamanipulation.sql
@@ -1,5 +1,5 @@
 USE silex;
 
 -- PREFILL YOUR TABLES HERE
-INSERT INTO blog_post (title, text) VALUES ('New Title', 'This is the content'yy);
+INSERT INTO blog_post (title, text) VALUES ('New Title', 'This is the content');
 INSERT INTO usrpwd (username, password) VALUES ('admin', '0000')
